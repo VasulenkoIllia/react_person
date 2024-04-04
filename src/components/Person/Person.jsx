7f797eb6426599354fafe6cc92cf5826faf342f6
Illia Vasulenko <vasulenko.illia@gmail.com>
2024-04-04 21:04:56 +0300

    fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -18,8 +18,8 @@ function checkData(person) {
 
 export const Person = ({ person }) => (
   <section className="Person">
-    <h2 className="Person__name">{`${person.name}`}</h2>
-    <p className="Person__age">{`${person.age ? person.age : ''}`}</p>
+    <h2 className="Person__name">{`My name is  ${person.name}`}</h2>
+    {person.age ? <p className="Person__age">{`I am  ${person.age}`}</p> : ''}
     <p className="Person__partner">{checkData(person)}</p>
   </section>
 );
