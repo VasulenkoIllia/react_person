32543311d46e05f7565c44821de46fd7f87dedf3
Illia Vasulenko <vasulenko.illia@gmail.com>
2024-04-04 20:50:45 +0300

    solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,25 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+function checkData(person) {
+  if (person.isMarried === false) {
+    return 'I am not married';
+  }
+
+  if (person.isMarried === true && person.partnerName && person.sex === 'm') {
+    return `${person.partnerName} is my wife`;
+  }
+
+  if (person.isMarried === true && person.partnerName && person.sex === 'f') {
+    return `${person.partnerName} is my husband`;
+  }
+
+  return true;
+}
+
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">{`${person.name}`}</h2>
+    <p className="Person__age">{`${person.age ? person.age : ''}`}</p>
+    <p className="Person__partner">{checkData(person)}</p>
+  </section>
+);
