b1200e2cefaad8c1667f97973c2c56a94a7825d4
Illia Vasulenko <vasulenko.illia@gmail.com>
2024-04-04 21:31:28 +0300

    fix age

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    {person.age ? <p className="Person__age">{`I am  ${person.age}`}</p> : ''}
+    {person.age ? <p className="Person__age">{`I am ${person.age}`}</p> : ''}
     <p className="Person__partner">
       {/* eslint-disable-next-line no-nested-ternary */}
       {person.isMarried
