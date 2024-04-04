0b9c58cc233e18e98d3ce6c58273993bbd6caa52
Illia Vasulenko <vasulenko.illia@gmail.com>
2024-04-04 21:25:21 +0300

    delete function

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,25 +1,16 @@
 import React from 'react';
 
-function checkData(person) {
-  if (person.isMarried === false) {
-    return 'I am not married';
-  }
-
-  if (person.isMarried === true && person.partnerName && person.sex === 'm') {
-    return `${person.partnerName} is my wife`;
-  }
-
-  if (person.isMarried === true && person.partnerName && person.sex === 'f') {
-    return `${person.partnerName} is my husband`;
-  }
-
-  return true;
-}
-
 export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">{`My name is ${person.name}`}</h2>
     {person.age ? <p className="Person__age">{`I am  ${person.age}`}</p> : ''}
-    <p className="Person__partner">{checkData(person)}</p>
+    <p className="Person__partner">
+      {/* eslint-disable-next-line no-nested-ternary */}
+      {person.isMarried
+        ? person.sex === 'm'
+          ? `${person.partnerName} is my wife`
+          : `${person.partnerName} is my husband`
+        : 'I am not married'}
+    </p>
   </section>
 );
